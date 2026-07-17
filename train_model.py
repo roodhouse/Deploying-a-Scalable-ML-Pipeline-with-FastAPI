0ff6de1529ce27689c084de35dda61a92b57658e
roodhouse <rughjm@gmail.com>
2026-07-17 11:17:29 -0500

    Load census.csv in train_model using project-relative path
    Set project_path from this file location and read data/census.csv with pandas.

--- train_model.py
+++ train_model.py
@@ -12,11 +12,11 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the census.csv data
-project_path = "Your path here"
+# Load the census.csv data
+project_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
